3f960b2f3a9005fdcb2c779ab62363c58f855018
Type your name here <firstname.lastname@mail.utoronto.ca>
2021-10-13 19:33:13 -0400

    Journal entry and controller class added

---
+++ src/main/java/Journal/JournalController.java
@@ -0,0 +1,29 @@
+package Journal;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+public class JournalController {
+
+    // the object that the controller instructs
+    private JournalManager journalmanager;
+
+    public JournalController(){
+        this.journalmanager = new JournalManager();
+    }
+    /**
+     * Calls journal manager to create entry with the given information.
+     *
+     * @param title The title of the entry.
+     * @param content The content of the entry.
+     * @param date The date the entry was written on.
+     * @param tags The tags we want to give the entry.
+
+     */
+    public String callCreateEntry(String title, String content, LocalDate date, ArrayList<String> tags){
+
+        return journalmanager.createEntry(title, content, date, tags);
+    }
+
+}
---
+++ src/main/java/Journal/JournalEntry.java
@@ -0,0 +1,70 @@
+package Journal;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+
+public class JournalEntry {
+
+    // The title of the entry
+    private String title;
+    // the date the entry was written on
+    private LocalDate date;
+    // the tags associated with the entry
+    private  ArrayList<String> tags;
+    // the content within the entry
+    private String content;
+
+    /**
+     * Creates a new journal entry with the given
+     * entry, tags and the date the entry was written.
+     *
+     * @param entry        the content of the journal entry.
+     * @param tags          tags associated with journal entry.
+     * @param date the date the journal entry was written.
+     */
+
+
+    public JournalEntry(String title, String entry, ArrayList<String> tags, LocalDate date){
+        this.title = title;
+        this.content = entry;
+        this.tags = tags;
+        this.date = date;
+    }
+    /**
+     * Reports the title of this entry.
+     *
+     * @return the title of this entry.
+     */
+    public String titleGetter(){
+        return this.title;
+    }
+
+    /**
+     * Reports the content of this entry.
+     *
+     * @return the content of this entry.
+     */
+    public String contentGetter(){
+        return this.content;
+    }
+    /**
+     * Reports the tags associated with this entry.
+     *
+     * @return the tags of this entry.
+     */
+    public ArrayList<String> tagsGetter(){
+        return this.tags;
+    }
+    /**
+     * Reports the date of this entry.
+     *
+     * @return the date of this entry.
+     */
+    public LocalDate dateGetter(){
+        return this.date;
+    }
+
+}
