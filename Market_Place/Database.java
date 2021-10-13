d5e46ffe0a5f147773b1f9ffc7c07308305b50af
annielin11 <annieen.lin@mail.utoronto.ca>
2021-10-12 20:09:29 -0400

    The first version of my codes: Database, Database_Manager, and Item

---
+++ Market_Place/Database.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Database {
+    /**
+     * A database that stores a list of items from Database_Manager
+     *
+     *
+     * @param item_lst the list of items that are posts
+     *
+     */
+
+//    we need to create an id that makes the items in the list unique
+
+
+    public ArrayList item_lst;
+
+    public Database(){
+        /**
+         * The constructor for Database and initializes an empty array list
+         * */
+        this.item_lst = new ArrayList();
+    }
+
+    public void store_item(){
+
+        /**
+         * Gets the post/item from Database_Manager and stores them
+         * into item_lst
+         */
+//        Currently not sure how we are going to get the items yet so I am not implementing them yet
+
+    }
+
+}
---
+++ Market_Place/Item.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Item {
+
+    /**
+     * An item class that stores the users' information and the description of the item
+     *
+     * @param item_descript the description of the item
+     */
+
+    public String item_descript;
+
+//    How are we getting the user's input?
+//    I need to know where and what method gets the user's info in order to implement user's info
+    public Item(){
+        this.item_descript = null;
+    }
+
+    public String Item_Descript(){
+        /**
+         * Get the description of the item through Database_Manager
+         */
+//        I have the description of the item in Database_Manager, but I am not sure how to retrieve it yet
+
+    public void delete_item(){
+//        Not sure where to ask password, why do we need this if we are deleting through Database_Manager
+    }
+
+
+}
