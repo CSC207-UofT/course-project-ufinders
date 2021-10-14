a5ca1905b0454f0a063d2aee7b1603c08823ea74
m.kovacs02 <martonk.kovacs02@gmail.com>
2021-10-14 15:54:57 -0400

    Build Initial version of skeleton code. NOT FINAL. Will update for fixes and clarifications. NOT FINAL.

--- src/main/Alarm_Maker.java
+++
@@ -1,8 +0,0 @@
-public class Alarm_Maker {
-
-    public static void creator(String title, String filename, Event event){
-        Alarm alarm = new Alarm(title, filename);
-        event.setAlarm(alarm);
-    }
-
-}
