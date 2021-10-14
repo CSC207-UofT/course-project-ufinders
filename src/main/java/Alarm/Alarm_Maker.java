1a61e6c440b72e56e3c11bf213efb5cb62f98cc4
m.kovacs02 <martonk.kovacs02@gmail.com>
2021-10-14 16:05:45 -0400

    Updated File Structure. Waiting on others to finalize code

---
+++ src/main/java/Alarm/Alarm_Maker.java
@@ -0,0 +1,10 @@
+package java.Alarm;
+
+public class Alarm_Maker {
+
+    public static void creator(String title, String filename, Event event){
+        Alarm alarm = new Alarm(title, filename);
+        event.setAlarm(alarm);
+    }
+
+}
---
+++ src/main/java/User/User_Controller.java
@@ -0,0 +1,14 @@
+package java.User;
+
+import java.util.Date;
+
+public class User_Controller {
+
+    public static void addtousercalendar(Date date, String time, String title, String user, Boolean print){
+        EventManager.addCourseEvent(date, time, title, user, print);
+    }
+
+    public static void usercreate(String userID, String name){
+        Create_User.makeUserObject(userID, name);
+    }
+}
