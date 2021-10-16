41aa1798df4c1f6221b30aa7ec892576920cf7ca
clairelp <90481417+clairelp@users.noreply.github.com>
2021-10-15 20:04:14 -0400

    This is a central hub to link self-care and market

---
+++ src/main/java/MainController.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MainController {
+
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu() {
+        Scanner input = new Scanner(System.in);
+        boolean stop = false;
+        while (!stop) {
+            System.out.println("Welcome to Ufind! Would you like to enter the self-help section, enter the marketplace, or exit? (self-help/market/exit)");
+            String choice = input.nextLine();
+            if (choice.equals("market")) {
+                User_controls.intro();
+            } else if (choice.equals("self_help")) {
+                new User.User_UI();
+            } else if (choice.equals("exit")) {
+                stop = true;
+            }
+        }
+    }
