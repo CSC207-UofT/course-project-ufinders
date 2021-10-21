dd1e89a830936806015c3baf91f8045c24bf6cee
Thaksha <thaksha.mangaleswaran@mail.utoronto.ca>
2021-10-21 18:09:02 -0400

    rough delete window

--- src/main/java/Journal/JournalWindow.java
+++ src/main/java/Journal/JournalWindow.java
@@ -1,39 +1,60 @@
 package Journal;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
 import java.io.IOException;
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class JournalWindow {
+public class JournalWindow extends WindowAdapter implements ItemListener {
+    Choice entry = new Choice();
+    HashMap<String, File> files = new HashMap<>();
+    Frame Journal = new Frame("Entry Deletion");
+    public JournalWindow() {
 
-    public static void main(String[] args) throws IOException {
-        JournalController controller = new JournalController();
 
-        JTextField titleInput = new JTextField();
-        JTextField entryInput = new JTextField();
-        JTextField tagsInput = new JTextField();
 
-        Object[] prompts = {
-                "Title:", titleInput,
-                "Type you journal entry:", entryInput,
-                "Type the tags you would like to add to entry seperated by commas :", tagsInput,
-        };
-        int option = JOptionPane.showConfirmDialog(null, prompts, "Journal Entry", JOptionPane.OK_CANCEL_OPTION);
-        String title = titleInput.getText();
-        String entry = entryInput.getText();
-        String[] allTags = tagsInput.getText().split(",");
-        LocalDate today = LocalDate.now();
 
-        // strip all tags of any whitespace
-        for( int i = 0; i < allTags.length; i += 1 ){
-            allTags[i] = allTags[i].strip();
+
+
+        Journal.setSize(700, 400);
+        Journal.setVisible(true);
+
+
+
+        File JournalEntries = new File("/Users/thakshamangalam/Documents/JournalEntries");
+        File[] directoryListing = JournalEntries.listFiles();
+
+
+        for (File journalEntry : directoryListing) {
+            files.put(journalEntry.getName(), journalEntry);
+            entry.add(journalEntry.getName());
         }
+        Journal.add(entry);
+        entry.addItemListener(this);
+    }
+    @Override
+        public void itemStateChanged(ItemEvent e) {
 
-        // ask controller to add entry to journal
-       controller.callCreateEntry(title, entry, today, allTags);
+        String entryToDelete = entry.getSelectedItem();
+        entry.remove(entryToDelete);
+        files.get(entryToDelete).delete();
 
-       // String[] sample = {"apple", "birthday"};
-       // controller.callCreateEntry("birthday", "ordering air pods", today, sample);
 
 
     }
+
+    public static void main(String[] args) {
+        new JournalWindow();
+
+    }
+
+
+
+
+
+
+
 }
