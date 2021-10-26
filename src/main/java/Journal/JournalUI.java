073c561ee8a8771762695d01b922336a77869e89
Thaksha <thaksha.mangaleswaran@mail.utoronto.ca>
2021-10-25 21:35:55 -0400

    implemented create database on users computer

--- src/main/java/Journal/JournalUI.java
+++ src/main/java/Journal/JournalUI.java
@@ -1,4 +1,21 @@
 package Journal;
 
+import javax.swing.filechooser.FileSystemView;
+import java.nio.file.Paths;
+
 public class JournalUI {
+    public JournalController controller;
+
+    public JournalUI(){
+        MakeDir dir = new MakeDir(FileSystemView.getFileSystemView()
+                        .getHomeDirectory()
+                        .getAbsolutePath() + "/" +"Documents" + "/"  + "Journal Entries");
+        this.controller = new JournalController(dir);
+    }
+
+    public static void main(String[] args) {
+    JournalUI jc= new JournalUI();
+    }
 }
+
+
