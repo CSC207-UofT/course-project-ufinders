f4d51662e842e06695c6a04bbe8b430f2306d24c
annielin11 <annieen.lin@mail.utoronto.ca>
2021-11-23 00:01:43 -0500

    Fixed the dangling comments

--- src/main/java/Marketplace/ItemManager.java
+++ src/main/java/Marketplace/ItemManager.java
@@ -19,57 +19,64 @@ public class ItemManager {
      *
      */
 
+    public ItemManager(){}
+
+    /**
+     * Creates Animal Item and store into Database
+     */
     public void CreatePostAnimal(String name, String description, double price, String contact,
                                  String password, String email, Item.campus campus, String animal_type){
-        /**
-         * Creates Animal Item and store into Database
-         */
+
         ItemCreator itemCreator = new ItemCreator();
         Animal item = (Animal) itemCreator.makeItem(ItemCategories.animal);
         item.edit(name, description, contact, email, password, price, campus, animal_type);
         Database.StoreItem(item);
     }
 
+    /**
+     * Creates Clothes Item and store into Database
+     */
     public void CreatePostClothes(String name, String description, double price, String contact,
                                   String password, String email, Item.campus campus, Clothes.size size,
                                   Item.condition condition){
-        /**
-         * Creates Clothes Item and store into Database
-         */
+
         ItemCreator itemCreator = new ItemCreator();
         Clothes item = (Clothes) itemCreator.makeItem(ItemCategories.clothes);
         item.edit(name, description, contact, email, password, price, campus, size, condition);
         Database.StoreItem(item);
     }
 
+    /**
+     * Creates Electronic Item and store into Database
+     */
     public void CreatePostElectronic(String name, String description, double price, String contact, String email,
                                      String password, Item.campus campus, Item.condition condition,
                                      String tech_specifications){
-        /**
-         * Creates Electronic Item and store into Database
-         */
+
         ItemCreator itemCreator = new ItemCreator();
         Electronic item = (Electronic) itemCreator.makeItem(ItemCategories.electronics);
         item.edit(name, description, contact, email, password, price, campus, condition, tech_specifications);
         Database.StoreItem(item);
     }
 
+    /**
+     * Creates Miscellaneous Item and store into Database
+     */
     public void CreatePostMisc(String name, String description, double price, String contact, String email,
                                String password, Item.campus campus){
-        /**
-         * Creates Miscellaneous Item and store into Database
-         */
+
         ItemCreator itemCreator = new ItemCreator();
         Misc item = (Misc) itemCreator.makeItem(ItemCategories.misc);
         item.edit(name, description, contact, email, password, price, campus);
         Database.StoreItem(item);
     }
 
+    /**
+     * Creates Textbook Item and store into Database
+     */
     public void CreatePostTextbook(String name, String description, double price, String contact, String email,
                                    String password, Item.campus campus, String course) {
-        /**
-         * Creates Textbook Item and store into Database
-         */
+
         ItemCreator itemCreator = new ItemCreator();
         Textbook item = (Textbook) itemCreator.makeItem(ItemCategories.textbook);
         item.edit(name, description, contact, email, password, price, campus, course);
@@ -77,12 +84,12 @@ public class ItemManager {
 
     }
 
-
+    /**
+     * Checks to see if the password given by
+     * user matches with the item in Database
+     */
     public static boolean password_match(String title, String password){
-        /**
-         * Checks to see if the password given by
-         * user matches with the item in Database
-         */
+
         ArrayList<Item> item_lst = Database.GetLst();
 
         for (Item item: item_lst){
@@ -95,16 +102,20 @@ public class ItemManager {
     }
 
 //    TODO: figure how to delete item Database
+    /**
+     * Removes the item sold from post and return true. Needs to check the password before the item can be removed.
+     */
     public static boolean remove_post(String title, String password){
-        /**
-         * Removes the item sold from post and return true. Needs to check the password before the item can be removed.
-         */
+
         ArrayList<Item> posts = Database.GetLst();
 
         for (Item item: posts){
             if (item.getName().equals(title) & password_match(title, password)){
                 posts.remove(item);
                 return true;
+            }
+            else{
+                Database.StoreItem(item);
             }
         }
         return false;
