84bca03cfe2a1577a01d12cd8af3d84d9dccb944
annielin11 <annieen.lin@mail.utoronto.ca>
2021-12-03 22:03:22 -0500

    Is able to select items and see the information. Done in MarketplaceWindow class

--- src/main/java/Marketplace/MarketplaceWindow.java
+++ src/main/java/Marketplace/MarketplaceWindow.java
@@ -6,6 +6,9 @@ import Marketplace.Items.types.Electronic;
 import Marketplace.Items.types.Item;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class MarketplaceWindow {
@@ -21,24 +24,49 @@ public class MarketplaceWindow {
                 null, JOptionPane.QUESTION_MESSAGE);
     }
 
+    /**
+     * Displays the list of items that are on sale on popup window
+     */
     public static void displayItems(ArrayList<Item> items){
         DefaultListModel<String> listModel = new DefaultListModel<>();
         JFrame jFrame = new JFrame();
+//       this closes the whole program so we have to fix it to go somewhere else
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         int curr_item = 0;
         while(curr_item < items.size()){
-            listModel.addElement((curr_item + 1) + ". " + items.get(curr_item).getName() + ": " +
-                            items.get(curr_item).getItem_description() + ", for $" +
+            listModel.addElement((curr_item + 1) + ". " + items.get(curr_item).getName() + ", for $" +
                     items.get(curr_item).getPrice());
             curr_item++;
         }
         JList<String> jList = new JList<>(listModel);
-        jList.setBounds(0, 0, 400, 400);
+        jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        jList.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                int ind = jList.getSelectedIndex();
+                displayItemInfo(items.get(ind));
+            }
+        });
+        jList.setBounds(0, 0, 700, 700);
         jFrame.add(jList);
-        jFrame.setSize(400,400);
+        jFrame.setSize(700,700);
         jFrame.setLocationRelativeTo(null);
-        jFrame.setLayout(null);
         jFrame.setVisible(true);
 
+
+    }
+
+    /**
+     * Displays the item's information on new popup window when the item is clicked on in displayItems
+     */
+    private static void displayItemInfo(Item item){
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        JLabel label = new JLabel();
+        label.setText(item.getName());
+        frame.setSize(500, 300);
+        frame.add(label);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
     }
 
