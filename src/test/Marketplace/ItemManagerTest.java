ab45a47ffc9c0e370ce684dd03bb5c7042915674
annielin11 <annieen.lin@mail.utoronto.ca>
2021-12-05 18:28:16 -0500

    need to commit to pull

--- src/test/Marketplace/ItemManagerTest.java
+++ src/test/Marketplace/ItemManagerTest.java
@@ -1,49 +1,61 @@
-//package Marketplace;
-//
-//import Marketplace.Items.types.*;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.util.ArrayList;
-//
-//public class ItemManagerTest {
-//    ItemManager itemManager = new ItemManager();
-//    String pass = "123";
-//    String contact = "12345";
-//    String email = "123@gmail.com";
-//
-//    @Test(timeout = 1000)
-//    public void testCreatePosts(){
-//        itemManager.CreatePostAnimal("Cat", "Furry", 105, contact,
-//                pass, email, Item.campus.UTSG, "Cheetah");
-//        itemManager.CreatePostClothes("Tshirt", "Red", 10, contact,
-//                pass, email, Item.campus.UTSG, Clothes.size.S, Item.condition.LikeNew);
-//        itemManager.CreatePostElectronic("Laptop", "Mac", 500, contact,
-//                pass, email, Item.campus.UTSC, Item.condition.LikeNew, "Mac Pro");
-//        itemManager.CreatePostMisc("cup", "blue", 2, contact,
-//                pass, email, Item.campus.UTSG);
-//        itemManager.CreatePostTextbook("Math", "MAT123 textbook", 10, contact,
-//                pass, email, Item.campus.UTM, "Algebra");
-//        ArrayList<Item> lst = Database.GetLst();
-//        assert (lst.get(0) instanceof Animal);
-//        assert (lst.get(1) instanceof Clothes);
-//        assert (lst.get(2) instanceof Electronic);
-//        assert (lst.get(3) instanceof Misc);
-//        assert (lst.get(4) instanceof Textbook);
-//    }
-//
-//
-////    @Test(timeout = 500)
-////    public void testpassword_match(){
-////        itemManager.CreatePostElectronic("Phone", "Mac", 500, contact,
-////                pass, email, Item.campus.UTSC, Item.condition.LikeNew, "Mac Pro");
-////        itemManager.CreatePostMisc("Bowl", "blue", 2, contact,
-////                pass, email, Item.campus.UTSG);
-////        ArrayList<Item> items = Database.GetLst();
-////        assert (!items.isEmpty());
-////        assert ItemManager.password_match("Phone", "123");
-////    }
-//
-//
-//
-//}
+package Marketplace;
+
+import Marketplace.Items.types.*;
+import org.junit.Test;
+
+
+public class ItemManagerTest {
+    ItemManager itemManager = new ItemManager();
+    String pass = "123";
+    String contact = "12345";
+    String email = "123@gmail.com";
+
+
+    /**
+     * Check to see the items are stored
+     */
+    @Test(timeout = 1000)
+    public void testCreatePosts(){
+        itemManager.CreatePostAnimal("Cat", "Furry", 105, contact,
+                pass, email, Item.campus.UTSG, "Cheetah");
+        itemManager.CreatePostClothes("Shirt", "Red", 10, contact,
+                pass, email, Item.campus.UTSG, Clothes.size.S, Item.condition.LikeNew);
+        itemManager.CreatePostElectronic("Laptop", "Mac", 500, contact,
+                pass, email, Item.campus.UTSC, Item.condition.LikeNew, "Mac Pro");
+        itemManager.CreatePostMisc("cup", "blue", 2, contact,
+                pass, email, Item.campus.UTSG);
+        itemManager.CreatePostTextbook("Math", "MAT123 textbook", 10, contact,
+                pass, email, Item.campus.UTM, "Algebra");
+        assert (!Database.item_type_lst.isEmpty());
+    }
+
+
+    /**
+     * test if password matches
+     */
+    @Test(timeout = 500)
+    public void testPasswordMatch(){
+        itemManager.CreatePostElectronic("Laptop", "Mac", 500, contact,
+                pass, email, Item.campus.UTSC, Item.condition.LikeNew, "Mac Pro");
+        assert (!ItemManager.password_match("", ""));
+        assert (!ItemManager.password_match("laptop", "1234"));
+        assert (!ItemManager.password_match("phone", "123"));
+    }
+
+    /**
+     * test that removePost can remove item and that only the item that needs to be removed is removed
+     */
+    @Test(timeout = 500)
+    public void testRemovePost(){
+        Electronic phone = new Electronic();
+        phone.edit("phone", "shiny", "123", "123", "123", 1000,
+                Item.campus.UTM, Item.condition.LikeNew, "iphone");
+        Database.StoreItem(phone);
+        assert (!ItemManager.removePost("laptop", "123"));
+        assert (ItemManager.removePost("phone", "123"));
+        assert (!ItemManager.removePost("phone", "123"));
+    }
+
+
+
+}
